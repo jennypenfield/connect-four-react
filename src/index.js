158d517c844f04ebb1f93e673c72bb478f972550
jennypenfield <jipenfield11@gmail.com>
2017-06-10 09:25:53 -0500

    added collaborative class code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,67 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import Board from './Board'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+const showStateExplorer = document.location.search.indexOf('stateexplorer') !== -1
+
+const emptyBoard = [
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null]
+]
+
+const initialState = {
+  turn: true,
+  board: emptyBoard
+}
+
+function StateExplorer (state) {
+  const stateJSON = JSON.stringify(state, null, 2)
+
+  return (
+    <section id='explorerContainer'>
+      <h1>State Explorer</h1>
+      <textarea value={stateJSON} />
+    </section>
+  )
+}
+
+function ConnectFour (state) {
+  return (
+    <section id='gameContainer'>
+      {Board(state.board)}
+    </section>
+  )
+}
+
+window.appState = initialState
+
+function App (state) {
+  let stateExplorerComponent = null
+  if (showStateExplorer) stateExplorerComponent = StateExplorer(state)
+
+  return (
+    <div id='appContainer'>
+      {ConnectFour(state)}
+      {stateExplorerComponent}
+    </div>
+  )
+}
+
+// ---------------------------------------------------------
+// Render Loop
+// ---------------------------------------------------------
+
+const rootEl = document.getElementById('root')
+
+function renderNow () {
+  ReactDOM.render(App(window.appState), rootEl)
+  window.requestAnimationFrame(renderNow)
+}
+
+window.requestAnimationFrame(renderNow)
